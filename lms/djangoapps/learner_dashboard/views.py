615dacde5006c608a1634c74474a4c846a3add0b
Renzo Lucioni <renzo@renzolucioni.com>
2016-06-14 17:09:07 -0400

    Link program listing cards to detail pages
    When program detail pages are enabled, cards on the listing page will link to their respective detail pages. Includes extensive cleanup of program listing tests. ECOM-4227.

--- lms/djangoapps/learner_dashboard/views.py
+++ lms/djangoapps/learner_dashboard/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import Http404
 from django.views.decorators.http import require_GET
+from django.utils.text import slugify
 
 from edxmako.shortcuts import render_to_response
 from openedx.core.djangoapps.credentials.utils import get_programs_credentials
@@ -21,28 +22,33 @@ from lms.djangoapps.learner_dashboard.utils import (
 @require_GET
 def view_programs(request):
     """View programs in which the user is engaged."""
-    show_program_listing = ProgramsApiConfig.current().show_program_listing
-    if not show_program_listing:
+    programs_config = ProgramsApiConfig.current()
+    if not programs_config.show_program_listing:
         raise Http404
 
     meter = utils.ProgramProgressMeter(request.user)
     programs = meter.engaged_programs
 
     # TODO: Pull 'xseries' string from configuration model.
-    marketing_root = urljoin(settings.MKTG_URLS.get('ROOT'), 'xseries').strip('/')
+    marketing_root = urljoin(settings.MKTG_URLS.get('ROOT'), 'xseries').rstrip('/')
+
     for program in programs:
+        if programs_config.show_program_details:
+            base = reverse('program_details_view', kwargs={'program_id': program['id']}).rstrip('/')
+            slug = slugify(program['name'])
+        else:
+            base = marketing_root
+            slug = program['marketing_slug']
+
+        program['detail_url'] = '{base}/{slug}'.format(base=base, slug=slug)
         program['display_category'] = utils.get_display_category(program)
-        program['marketing_url'] = '{root}/{slug}'.format(
-            root=marketing_root,
-            slug=program['marketing_slug']
-        )
 
     context = {
         'programs': programs,
         'progress': meter.progress,
-        'xseries_url': marketing_root if ProgramsApiConfig.current().show_xseries_ad else None,
+        'xseries_url': marketing_root if programs_config.show_xseries_ad else None,
         'nav_hidden': True,
-        'show_program_listing': show_program_listing,
+        'show_program_listing': programs_config.show_program_listing,
         'credentials': get_programs_credentials(request.user, category='xseries'),
         'disable_courseware_js': True,
         'uses_pattern_library': True
@@ -55,8 +61,8 @@ def view_programs(request):
 @require_GET
 def program_details(request, program_id):
     """View details about a specific program."""
-    show_program_details = ProgramsApiConfig.current().show_program_details
-    if not show_program_details:
+    programs_config = ProgramsApiConfig.current()
+    if not programs_config.show_program_details:
         raise Http404
 
     program_data = utils.get_programs(request.user, program_id=program_id)
@@ -65,7 +71,6 @@ def program_details(request, program_id):
         raise Http404
 
     program_data = utils.supplement_program_data(program_data, request.user)
-    show_program_listing = ProgramsApiConfig.current().show_program_listing
 
     urls = {
         'program_listing_url': reverse('program_listing_view'),
@@ -77,7 +82,7 @@ def program_details(request, program_id):
     context = {
         'program_data': program_data,
         'urls': urls,
-        'show_program_listing': show_program_listing,
+        'show_program_listing': programs_config.show_program_listing,
         'nav_hidden': True,
         'disable_courseware_js': True,
         'uses_pattern_library': True
